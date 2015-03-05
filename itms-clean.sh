00fa7a909a62f553d23ba27cd8f66c79e42de5ee
Guillaume Khayat <guillaume.kh.alt@gmail.com>
2015-03-05 19:12:05 +0100

    Print list of files about to be modified before confirmation

--- itms-clean.sh
+++ itms-clean.sh
@@ -1,5 +1,16 @@
 #!/bin/bash  
 echo 'This will remove iTunes-specific personal metadata in all .m4a files in the current directory and subdirectories.'
+echo 'The following files will be modified :'
+echo ''
+dir=$(pwd)
+	for d in */ ; do
+		cd "$d"
+		for i in *.m4a; do
+			echo "$d""$i"
+		done
+		cd "$dir"
+	done
+echo ''
 read -p "Are you sure you want to continue? " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
