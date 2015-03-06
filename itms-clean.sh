d2cca48b8f8c71cd2950971469d3b9ac335d3263
Guillaume Khayat <guillaume.kh.alt@gmail.com>
2015-03-06 10:29:16 +0100

    Fixed indentation & added inline comments

--- itms-clean.sh
+++ itms-clean.sh
@@ -2,14 +2,16 @@
 echo 'This will remove iTunes-specific personal metadata in all .m4a files in the current directory and subdirectories.'
 echo 'The following files will be modified :'
 echo ''
+# Print list of files about to be modified
 dir=$(pwd)
-	for d in */ ; do
-		cd "$d"
-		for i in *.m4a; do
-			echo "$d""$i"
-		done
-		cd "$dir"
+for d in */ ; do
+	cd "$d"
+	for i in *.m4a; do
+		echo "$d""$i"
 	done
+	cd "$dir"
+done
+#Ask for confirmation
 echo ''
 read -p "Are you sure you want to continue? " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
@@ -18,6 +20,7 @@ then
 	for d in */ ; do
 		cd "$d"
 		for i in *.m4a; do
+			# Switch lines below to print all tags of all m4a files instead of changing them
 			atomicparsley "$i" --apID "" --purchaseDate "" --manualAtomRemove "moov.udta.meta.ilst.ownr" -W
 			# atomicparsley "$i" -t
 		done
